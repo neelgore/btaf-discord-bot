73b8774dfc16bb862567013bfea3f2b5839f9432
Neel Gore <neelgore@Neels-MacBook-Pro.local>
2021-04-02 23:48:04 -0700

    bot no longer writes a shame message; just reacts

--- src/btaf_bot.py
+++ src/btaf_bot.py
@@ -17,7 +17,6 @@ async def on_ready():
 			if emoji.name.casefold() == 'weirdchamp'.casefold():
 				client.WEIRDCHAMP = emoji
 	#find and save the weirdchamp emote
-	client.shamed_messages = {}
 
 @client.event
 async def on_message(message):
@@ -27,26 +26,31 @@ async def on_message(message):
 	emoji_names = {emoji.name.casefold(): emoji for emoji in message.guild.emojis}
 	#prepare dict of emote names to emote objets
 
-	if len(message.content.split()) == 1 \
-			and message.content.strip().casefold() in emoji_names and message.reference:
+	tokenized = message.content.split()
+
+	if len(tokenized) == 1 and message.content.strip().casefold() in emoji_names \
+		and message.reference is not None:
 		emoji = emoji_names[message.content.strip().casefold()]
-		original_message = await message.channel.fetch_message(message.reference.message_id)
-		await original_message.add_reaction(emoji)
-		await message.delete()
+		if emoji.animated:
+			original_message = await message.channel.fetch_message(message.reference.message_id)
+			await original_message.add_reaction(emoji)
+			await message.delete()
 		return
 	#replace replies with emote names with a reaction of the emote
 
-	violations = dict()
+	violations = False
 	animated = dict()
 	for word in message.content.split():
 		if word.casefold() in emoji_names and word.casefold() not in client.OK_WORDS:
 			if not emoji_names[word.casefold()].animated:
-				violations[word] = emoji_names[word.casefold()]
+				violations = True
 			else:
 				animated[word] = emoji_names[word.casefold()]
-	#populate animated and violations dicts
+	#populate animated and violations
 
-	if len(violations) == 0:
+	if violations:
+		await message.add_reaction(client.WEIRDCHAMP)
+	else:
 		if (len(animated) != 0):
 			new = " ".join((str(animated[w]) if w in animated else w) for w in message.content.split())
 			if len(new.split()) == 1:
@@ -56,21 +60,13 @@ async def on_message(message):
 				await message.channel.send(f'**{message.author.display_name}:** {new}')
 			await message.delete()
 			#simulate having Nitro
-	else:
-		await message.add_reaction(client.WEIRDCHAMP)
-		reply = await message.reply('\n'.join(
-			f'''Typing {wrong} instead of {str(emoji)} {str(client.WEIRDCHAMP)}''' \
-			for wrong, emoji in violations.items()))
-		client.shamed_messages[message.id] = reply.id
-		#shame and record message id
 
 @client.event
 async def on_message_edit(before, after):
-	if before.id in client.shamed_messages:
+	try:
 		await after.remove_reaction(client.WEIRDCHAMP, client.user)
-		old_shame = await before.channel.fetch_message(client.shamed_messages[before.id])
-		await old_shame.delete()
-		del client.shamed_messages[before.id]
+	except NotFound:
+		pass
 	await on_message(after)
 	#treat message edits as new messages
 
@@ -80,7 +76,7 @@ async def on_reaction_add(reaction, user):
 	#ignore bot's own reactions
 
 	if client.user in await reaction.users().flatten():
-		if reaction.message.id not in client.shamed_messages or reaction.emoji != client.WEIRDCHAMP:
+		if reaction.emoji != client.WEIRDCHAMP:
 			await reaction.message.remove_reaction(reaction.emoji, client.user)
 	#remove react once someone piggybacks, piggyback is a weirdchamp shame
 
