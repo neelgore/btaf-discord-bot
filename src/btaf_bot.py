18a6765425f22c4ad7746688b7a78bcfcb696040
Neel Gore <neelgore@Neels-MacBook-Pro.local>
2021-04-10 18:42:39 -0700

    halfway done with refactor; still functional

--- src/btaf_bot.py
+++ src/btaf_bot.py
@@ -1,7 +1,6 @@
 import os
 import discord
 import helpers
-import asyncio
 from dotenv import load_dotenv
 
 
@@ -27,62 +26,11 @@ async def on_message(message: discord.Message):
 	emoji_names = {emoji.name.casefold(): emoji for emoji in message.guild.emojis}
 	#prepare dict of emote names to emote objects
 
-	if helpers.message_is_animated_react(message, emoji_names):
-		emoji = emoji_names[message.content.casefold()]
-		original_message = await message.channel.fetch_message(message.reference.message_id)
-		await original_message.add_reaction(emoji)
-		await message.delete()
-		return
-	#replace replies with animated emote names with a reaction of the emote
+	await helpers.deal_with_emotes(message, emoji_names)
+	#replace messages with animated emotes with bot's version, because bot can use animated emotes
 
-	violations = False
-	animated = dict()
-	for word in message.content.split():
-		if word.casefold() in emoji_names and word.casefold() not in helpers.OK_WORDS:
-			if not emoji_names[word.casefold()].animated:
-				violations = True
-			else:
-				animated[word] = emoji_names[word.casefold()]
-	#populate animated and violations
-
-	if violations:
-		await message.add_reaction(helpers.find_emote(client, 'weirdchamp'))
-	else:
-		if (len(animated) != 0):
-			new_message = " ".join((str(animated[w]) if w in animated else w) \
-				for w in message.content.split())
-			if len(message.content.split()) == 1:
-				await message.channel.send(f'**{message.author.display_name}:**')
-				await message.channel.send(new_message)
-			else:
-				await message.channel.send(f'**{message.author.display_name}:** {new_message}')
-			await message.delete()
-			#delete original message, resend with animated emotes (because bot can use animated emotes)
-	if p_and_t := helpers.ping_and_time(message):
-		#if message attempts to schedule something (by pinging a role and giving a time)
-		roles, time_to_wait = p_and_t
-		if time_to_wait > 0:			
-			m, s = divmod(time_to_wait, 60)
-			h, m = divmod(m, 60)
-			schedule = 'Tentative ping scheduled in ' + str(int(h)) + (' hour ' if h == 1 else ' hours ') \
-				+ str(int(m)) + (' minute ' if m == 1 else ' minutes ') \
-				+ str(int(s)) + (' second.' if s == 1 else ' seconds.')
-			helpers.PINGS.add(message.id)
-			await message.reply(schedule)
-			await asyncio.sleep(time_to_wait)
-			if helpers.message_rsvps(message) and message.id in helpers.PINGS:
-				await message.reply(' '.join(role.mention for role in roles))
-			#ping the roles at the specified time if enough people rsvped
-			await asyncio.sleep(10*60)
-			#after 10 minute grace period, individually ping everyone who reacted but isn't in a voice channel
-			if message.id in helpers.PINGS:
-				if rsvps := helpers.message_rsvps(message):
-					to_be_pinged = {member for member in await rsvps.users().flatten()\
-						if not member.voice or not member.voice.channel	or member.voice.channel.guild != message.guild}
-					if len(to_be_pinged) > 0:
-						everyone = ' '.join(member.mention for member in to_be_pinged)
-						await message.reply(everyone + '\n' + str(emoji_names['modcheck']))
-				helpers.PINGS.remove(message.id)
+	await helpers.handle_ping_and_time(message, emoji_names)
+	#schedule and perform pings in the future
 
 @client.event
 async def on_message_edit(before: discord.Message, after: discord.Message):
@@ -104,9 +52,9 @@ async def on_reaction_add(reaction: discord.Reaction, user: discord.User):
 	#ignore bot's own reactions
 
 	if client.user in await reaction.users().flatten():
-		if reaction.emoji != helpers.find_emote(client, 'weirdchamp'):
+		if reaction.emoji.animated:
 			await reaction.message.remove_reaction(reaction.emoji, client.user)
-	#remove react once someone piggybacks, unless piggyback is a weirdchamp shame
+	#remove react if emote was animated, because it must have been a piggyback
 
 if __name__ == '__main__':
 	client.run(DISCORD_TOKEN)
--- src/helpers.py
+++ src/helpers.py
@@ -2,6 +2,7 @@ import re
 import discord
 import datetime
 import pytz
+import asyncio
 from functools import lru_cache
 from collections import defaultdict
 
@@ -20,11 +21,42 @@ def find_emote(client: discord.Client, name: str) -> discord.Emoji:
 			if emoji.name.casefold() == name.casefold():
 				return emoji
 
-def message_is_animated_react(message: discord.Message, emoji_names: {str: discord.Emoji}) -> bool:
-	'''checks if a message intends to react with an animated emote'''
+async def deal_with_emotes(message: discord.Message, emoji_names: {str: discord.Emoji}):
 	text = message.content.casefold()
-	return (text in emoji_names and message.reference is not None and emoji_names[text].animated \
-	and len(message.content.split()) == 1)
+	if text in emoji_names and message.reference is not None and emoji_names[text].animated \
+		and len(message.content.split()) == 1:
+		emoji = emoji_names[message.content.casefold()]
+		original_message = await message.channel.fetch_message(message.reference.message_id)
+		await original_message.add_reaction(emoji)
+		await message.delete()
+		return
+		#animated react job is done
+
+	violations = False
+	animated = {}
+	tokenized = message.content.split()
+	for word in tokenized:
+		if word.casefold() in emoji_names and word.casefold() not in OK_WORDS:
+			if not emoji_names[word.casefold()].animated:
+				violations = True
+				break
+			else:
+				animated[word] = emoji_names[word.casefold()]
+	#populate animated and violations
+
+	if violations:
+		await message.add_reaction(emoji_names['weirdchamp'.casefold()])
+	else:
+		if len(animated) > 0:
+			new_message = ' '.join((str(animated[word]) if word.casefold() in animated else word) \
+				for word in message.content.split())
+			if len(tokenized) == 1:
+				await message.channel.send(f'**{message.author.display_name}:**')
+				await message.channel.send(new_message)
+			else:
+				await message.channel.send(f'**{message.author.display_name}:** {new_message}')
+			await message.delete()
+			#delete original message, resend with animated emotes (because bot can use animated emotes)
 
 def ping_and_time(message: discord.Message) -> ([discord.Role], int) or None:
 	'''checks if a message contains a ping(s) AND a time.
@@ -39,8 +71,52 @@ def ping_and_time(message: discord.Message) -> ([discord.Role], int) or None:
 				now = datetime.datetime.now(pytz.timezone('America/Los_Angeles'))
 				time_to_wait = (now.replace(hour = hour if now.hour < 12 else hour + 12,
 					minute = minute, second = 0) - now).total_seconds()
-		if time_to_wait:
+		if time_to_wait and time_to_wait > 0:
 			return message.role_mentions, time_to_wait
+		else:
+			return None
+
+async def handle_ping_and_time(message: discord.Message, emoji_names: {str: discord.Emoji}):
+	time_to_wait = None
+	if len(message.role_mentions) > 0:
+		for word in message.content.split():
+			if match := re.match(TIME_REGEX, word):
+				if time_to_wait:
+					return
+					#multiple times in message
+				hour, minute = match.groups()
+				time_to_wait = seconds_until_time(int(hour), int(minute))
+	if time_to_wait is not None:
+		PINGS.add(message.id)
+		await message.reply(schedule_text(time_to_wait))
+		await asyncio.sleep(time_to_wait)
+		if message_rsvps(message) and message.id in PINGS:
+			await message.reply(' '.join(role.mention for role in message.role_mentions))
+			#ping the roles at the scheduled time if enough people rsvped
+		await asyncio.sleep(10*60)
+		#after 10 minute grace period, individually ping everyone who reacted but isn't in a voice channel
+		if (rsvps := message_rsvps(message)) and message.id in PINGS:
+			to_be_pinged = {member for member in await rsvps.users().flatten() \
+				if not member.voice or not member.voice.channel or member.voice.channel.guild != message.guild}
+			if len(to_be_pinged) > 0:
+				everyone = ' '.join(member.mention for member in to_be_pinged)
+				await message.reply(everyone + '\n' + str(emoji_names['modcheck']))
+			PINGS.remove(message.id)
+
+def schedule_text(time_to_wait: int) -> str:
+	m, s = divmod(time_to_wait, 60)
+	h, m = divmod(m, 60)
+	return 'Tentative ping scheduled in ' + str(int(h)) + (' hour ' if h == 1 else ' hours ') \
+		+ str(int(m)) + (' minute ' if m == 1 else ' minutes ') \
+		+ str(int(s)) + (' second.' if s == 1 else ' seconds.')
+				
+def seconds_until_time(hour: int, minute: int) -> int:
+	now = datetime.datetime.now(pytz.timezone('America/Los_Angeles'))
+	scheduled_time = now.replace(hour = hour, minute = minute, second = 0)
+	twelve_hours = datetime.timedelta(seconds=60*60*12)
+	while scheduled_time < now:
+		scheduled_time += twelve_hours
+	return (scheduled_time - now).total_seconds()
 
 def message_rsvps(message: discord.Message) -> [discord.Reaction]:
 	only_rsvps = [react for react in message.reactions if react.emoji.name.casefold() not in NOT_RSVPS \
